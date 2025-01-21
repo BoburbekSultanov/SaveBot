ffc1bea815dd901803f83a2e5f851cea938909b8
BoburbekSultanov <boburbeksultanov1@gmail.com>
2025-01-21 17:15:03 +0500

    add

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import bcrypt
 
 print(bcrypt.hashpw("1234".encode() , bcrypt.gensalt()))
+
+# with open('requirements.txt', 'r') as f:
+#     file = f.read().split()
+#     print(len(file))
--- web/app.py
+++ web/app.py
@@ -4,18 +4,25 @@ from starlette.middleware.sessions import SessionMiddleware
 from starlette_admin.contrib.sqla import Admin, ModelView
 
 from db import db
-from db.modles import Category
+from db.modles import User, Customer, Post, Job, Subjob, Employee, PostSubjob, SubjobEmployee
 from web.provider import UsernameAndPasswordProvider
 
 app = Starlette()
 admin = Admin(db._engine,
-              title="P28 Admin",
+              title="Kwork Admin",
               base_url= "/",
               auth_provider=UsernameAndPasswordProvider(),
               middlewares=[Middleware(SessionMiddleware, secret_key="sdgfhjhhsfdghn")]
               )
 
-admin.add_view(ModelView(Category))
+admin.add_view(ModelView(User))
+admin.add_view(ModelView(Customer))
+admin.add_view(ModelView(Post))
+admin.add_view(ModelView(Job))
+admin.add_view(ModelView(Subjob))
+admin.add_view(ModelView(Employee))
+admin.add_view(ModelView(PostSubjob))
+admin.add_view(ModelView(SubjobEmployee))
 
 admin.mount_to(app)
 
